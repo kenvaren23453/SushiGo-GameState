070013746c74b52bea71d99f8b8f32bd75699bd6
Nux28 <nuxoll28@up.edu>
2025-10-06 16:28:04 -0700

    primitive game state concept, incomplete. -josie

---
+++ app/src/main/java/edu/up/sushigogamestate/Player.java
@@ -0,0 +1,19 @@
+package edu.up.sushigogamestate;
+
+import java.util.ArrayList;
+
+/**
+ * A player in a Sushi Go game.
+ * (Note: We might be able to get away with not having player objects)
+ */
+public class Player {
+
+    //instance variables
+    private ArrayList<String> playedCards;
+
+    private ArrayList<String> passingHand;
+
+    private int playerID;
+
+    boolean ready; //whether player has pressed ready button
+}
